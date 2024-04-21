f0b00bfa753e3e67b825c44f8b4dc78fe3aea78f
Yasufumi Ogawa <yasufum.o@gmail.com>
2024-04-22 03:20:25 +0900

    Add devstack user setup script

---
+++ config/openstack/setup-stack-user.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+useradd -s /bin/bash -d /opt/stack -m stack
+echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
+
+# Permission of `stack` directory is 0700 on CentOS 8, but it cause an
+# error in a sanity check for the permission while running devstack
+# installatino.
+chmod 755 /opt/stack
+
+mkdir -p /opt/stack/.ssh
+#echo "#{ssh_pub_key}" >> /opt/stack/.ssh/authorized_keys
+chown -R stack:stack /opt/stack/.ssh
